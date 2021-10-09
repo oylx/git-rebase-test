c01b0e970a6547c94dadc7b391c59270ab7444c6
xiaomi <ouyanglinxia@xiaomi.com>
2021-10-09 17:18:37 +0800

    main

---  feat-test-3.js
+++
@@ -1,1 +0,0 @@
-let  featTest3 = 3
--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-let staging1 = 'staging1'
-let staging2 = 'staging2'
-let staging3 = 'staging3'
-let staging4 = 'staging4'
-let staging5 = 'staging5'
-
-let featTest1 = 1
+let main = 1
